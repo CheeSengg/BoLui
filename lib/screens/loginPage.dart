4a0215f20e0c39d01af5ca28fc191e77c2ce480a
Shaun <xshaun97@gmail.com>
2019-06-23 13:30:39 +0800

    added firebase login with email

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -7,11 +6,14 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => new _LoginPageState();
 }
 
+enum FormType { login, register }
+
 class _LoginPageState extends State<LoginPage> {
   final formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   bool validateAndSave() {
     final form = formKey.currentState;
@@ -23,9 +25,35 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  void validateAndSubmit() {
-    if(validateAndSave()) {
-      //Add firebase email auth here, start from new video
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+
+  void validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        if (_formType == FormType.login) {
+          FirebaseUser user = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: _email, password: _password);
+          print('signed in: ${user.uid}');
+        } else {
+          FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+          print('registered user: ${user.uid}');
+        }
+      } catch (e) {
+        print('error $e');
+      }
     }
   }
 
@@ -40,28 +68,54 @@ class _LoginPageState extends State<LoginPage> {
         child: new Form(
           key: formKey,
           child: new Column(
-            children: <Widget>[
-              new TextFormField(
-                decoration: new InputDecoration(labelText: "Email"),
-                validator: (value) =>
-                    value.isEmpty ? 'Email can\'t be empty' : null,
-                onSaved: (value) => _email = value,
-              ),
-              new TextFormField(
-                decoration: new InputDecoration(labelText: "Password"),
-                obscureText: true,
-                validator: (value) =>
-                    value.isEmpty ? 'Password can\'t be empty' : null,
-                onSaved: (value) => _password = value,
-              ),
-              new RaisedButton(
-                child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
-                onPressed: validateAndSubmit,
-              )
-            ],
+            children: buildInputs() + buildSubmitButtons(),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> buildInputs() {
+    return [
+      new TextFormField(
+        decoration: new InputDecoration(labelText: "Email"),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(labelText: "Password"),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formType == FormType.login) {
+      return [
+        new RaisedButton(
+          child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Create an account',
+              style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToRegister,
+        ),
+      ];
+    } else {
+      return [
+        new RaisedButton(
+          child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Have an account? Login',
+              style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToLogin,
+        ),
+      ];
+    }
   }
 }
