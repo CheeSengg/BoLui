d1c093ccd7b118b2cd30d9ca45d52b972b68f242
cheesengg <E0309953@u.nus.edu>
2019-06-24 20:48:28 +0800

    Improved login UI, Added Logout under settings, Auto SignIn function

--- lib/screens/root_page.dart
+++ lib/screens/root_page.dart
@@ -41,7 +41,7 @@ class _RootPageState extends State<RootPage>{
     super.initState();
     widget.auth.currentUser().then((userId){
       setState(() {
-//        authStatus = userId == null ? AuthStatus.notSignedIn : AuthStatus.signedIn;
+        authStatus = userId == null ? AuthStatus.notSignedIn : AuthStatus.signedIn;
       });
     });
   }
@@ -53,6 +53,7 @@ class _RootPageState extends State<RootPage>{
   }
 
   void _signedOut(){
+    currentTab = 0;
     setState(() {
       authStatus = AuthStatus.notSignedIn;
     });
